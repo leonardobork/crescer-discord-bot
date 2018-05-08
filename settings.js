5bdec4e1880ea86f5678ff7a92448930223250ff
Leonardo Bork <leonardo.bork@cwi.com.br>
2018-05-08 18:36:17 -0300

    asdasda

---
+++ settings.js
@@ -0,0 +1,12 @@
+const token = 'NDQyMDQxNjc5MzgyNDQ2MDkx.Dc5OEA.O6Fj_D0fDlHZz5ejJ6zrghxNURI'
+const db = {
+    host: 'ds151153.mlab.com:51153',
+    user: 'root',
+    password: 'root',
+    database: 'crescer-bot'
+}
+
+module.exports = {
+    token,
+    db
+}
